6b806c9616f9c637434e730b33c74b98162f3601
aasif247 <47910560+aasif247@users.noreply.github.com>
2021-07-25 15:31:41 +0600

    Store And Insert Method Added

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Student;
 use Illuminate\Http\Request;
 
 class StudentController extends Controller
@@ -9,5 +10,27 @@ class StudentController extends Controller
     
     public function index(){
         return view('crud');
+    }
+
+    //--------Store data-------------
+
+    public function store(Request $request){
+       $request->validate([
+          'name' => 'required',
+          'roll' => 'required',
+          'class' => 'required',
+       ],[
+          'name.required' => 'Please Input Your Name',
+          'roll.required' => 'Please Input Your Roll',
+          'class.required' => 'Please Input Your Class',
+       ]);
+
+       Student::insert([
+           'name' => $request->name,   
+           'roll' => $request->roll,   
+           'class' => $request->class,   
+       ]);
+
+       return redirect()->back()->with('success','Data Added Successfully');
     }
 }
