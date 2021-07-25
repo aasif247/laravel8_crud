f32b18e1db3510cff5c9845c76f9490f5c4d2c75
aasif247 <47910560+aasif247@users.noreply.github.com>
2021-07-25 20:36:44 +0600

    Edit and update method created

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -26,6 +26,8 @@ class StudentController extends Controller
           'class.required' => 'Please Input Your Class',
        ]);
 
+    //--------insert data------------
+
        Student::insert([
            'name' => $request->name,   
            'roll' => $request->roll,   
@@ -34,4 +36,37 @@ class StudentController extends Controller
 
        return redirect()->back()->with('success','Data Added Successfully');
     }
+
+    //-------student edit----------- 
+
+    public function edit($id){
+        $student = Student::findOrFail($id);
+        return view('edit',compact('student'));
+    }
+    
+    //-------student update-----------
+
+    public function update(Request $request,$id){
+
+        $request->validate([
+            'name' => 'required',
+            'roll' => 'required',
+            'class' => 'required',
+         ],[
+            'name.required' => 'Please Input Your Name',
+            'roll.required' => 'Please Input Your Roll',
+            'class.required' => 'Please Input Your Class',
+         ]);
+  
+
+         Student::findOrFail($id)->update([
+             'name' => $request->name,   
+             'roll' => $request->roll,   
+             'class' => $request->class,   
+         ]);
+  
+         return redirect()->to('/crud')->with('update','Data Updated Successfully');
+      }
+
+    
 }
